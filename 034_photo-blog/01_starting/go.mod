9b83df95c2c37bed664475aec371f3242ec01af8
liridon.rama <liridon.rama@me.com>
2020-06-01 09:09:05 +0200

    implemented file serving and uploading

---
+++ 034_photo-blog/01_starting/go.mod
@@ -0,0 +1,10 @@
+module example
+
+go 1.14
+
+require (
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/satori/go.uuid v1.2.0
+	github.com/teris-io/shortid v0.0.0-20171029131806-771a37caa5cf
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+)
--- 034_photo-blog/01_starting/main.go
+++ 034_photo-blog/01_starting/main.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
+	"io"
+	"log"
 	"net/http"
+	"os"
+	"strings"
+
+	uuid "github.com/satori/go.uuid"
+	"github.com/teris-io/shortid"
 )
 
 var tpl *template.Template
@@ -13,10 +21,68 @@ func init() {
 
 func main() {
 	http.HandleFunc("/", index)
+	http.HandleFunc("/upload", handleUpload)
+	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("images/"))))
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":8080", nil)
 }
 
+func handleUpload(w http.ResponseWriter, req *http.Request) {
+	err := req.ParseMultipartForm(5 * 1024 * 1024)
+
+	file, header, err := req.FormFile("file")
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer file.Close()
+
+	xF := strings.Split(header.Filename, ".")
+
+	sID, err := shortid.Generate()
+	if err != nil {
+		panic(sID)
+	}
+
+	fN := sID + "." + xF[1]
+	fmt.Println(fN)
+
+	localFile, err := os.Create("./images/" + fN)
+	if err != nil {
+
+		log.Panicln(err)
+	}
+	defer localFile.Close()
+
+	io.Copy(localFile, file)
+
+	c, err := req.Cookie("session")
+	if err != nil {
+		log.Panicln(err)
+	}
+	xS := strings.Split(c.Value, "|")
+	xS = append(xS, fN)
+	c.Value = strings.Join(xS, "|")
+
+	http.SetCookie(w, c)
+	http.Redirect(w, req, "/", 302)
+}
+
 func index(w http.ResponseWriter, req *http.Request) {
-	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+	c := getCookie(w, req)
+
+	xs := strings.Split(c.Value, "|")
+	tpl.ExecuteTemplate(w, "index.gohtml", xs)
+}
+
+func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
+	c, err := req.Cookie("session")
+	if err != nil {
+		sID := uuid.NewV4()
+		c = &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(w, c)
+	}
+	return c
 }
